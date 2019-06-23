237e15e425df63c579d8c063f3d1a47733883d42
Joey Zhou <jfzhou.nc@gmail.com>
2019-06-23 17:25:10 -0400

    complete section 2

--- App.js
+++ App.js
@@ -2,34 +2,51 @@ import React from 'react';
 import styled from 'styled-components';
 import Card from './components/Card';
 import { ScrollView, SafeAreaView } from 'react-native';
+import { NotificationIcon } from './components/Icons';
+import Logo from './components/Logo';
 
 export default function App() {
   return (
     <Container>
       <SafeAreaView>
-        <ScrollView>
+        <ScrollView style={{ height: '100%' }}>
           <TitleBar>
             <Avatar source={require('./assets/avatar.jpg')} />
             <Title>Welcome back,</Title>
             <Name>Meng</Name>
+            <NotificationIcon style={{ position: 'absolute', right: 20, top: 5 }} />
           </TitleBar>
+          <ScrollView
+            style={{ flexDirection: 'row', padding: 20, paddingLeft: 12, paddingTop: 30 }}
+            horizontal={true}
+          >
+            <Logo image={require('./assets/logo-framerx.png')} text="Framer-x" />
+            <Logo image={require('./assets/logo-figma.png')} text="Figma" />
+          </ScrollView>
+
+          <Subtitle>Continue Learning</Subtitle>
+          <ScrollView
+            horizontal={true}
+            style={{ paddingBottom: 30 }}
+            showsHorizontalScrollIndicator={false}
+          >
+            <Card
+              title="Styled Components"
+              image={require('./assets/background2.jpg')}
+              caption="React Native"
+              logo={require('./assets/logo-react.png')}
+              subtitle="5 of 12 sections"
+            />
+            <Card
+              title="Styled Components"
+              image={require('./assets/background2.jpg')}
+              caption="React Native"
+              logo={require('./assets/logo-react.png')}
+              subtitle="5 of 12 sections"
+            />
+          </ScrollView>
         </ScrollView>
       </SafeAreaView>
-
-      <Subtitle>Continue Learning</Subtitle>
-      <ScrollView
-        horizontal={true}
-        style={{ paddingBottom: 30 }}
-        showsHorizontalScrollIndicator={false}
-      >
-        <Card
-          title="Styled Components"
-          image={require('./assets/background2.jpg')}
-          caption="React Native"
-          logo={require('./assets/logo-react.png')}
-          subtitle="5 of 12 sections"
-        />
-      </ScrollView>
     </Container>
   );
 }
@@ -73,6 +90,6 @@ const Subtitle = styled.Text`
   font-weight: 600;
   font-size: 15px;
   margin-left: 20px;
-  margin-top: 50px;
+  margin-top: 20px;
   text-transform: uppercase;
 `;
