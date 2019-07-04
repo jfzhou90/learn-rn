5bbc5d068b2b6bc81cb6062f28dcdd2a60d9137e
Joey Zhou <jfzhou.nc@gmail.com>
2019-07-03 22:03:36 -0400

    complete part 1

--- components/Course.js
+++ components/Course.js
@@ -7,7 +7,7 @@ const screenWidth = Dimensions.get('window').width;
 function getCourseWidth(screenWidth) {
   var cardWidth = screenWidth - 40;
   if (screenWidth >= 768) {
-    cardWidth = (screenWidth - 60) / 2;
+    cardWidth = (screenWidth - 60)  / 2;
   }
   if (screenWidth >= 1024) {
     cardWidth = (screenWidth - 80) / 3;
