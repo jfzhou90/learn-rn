6a07094d031a21a03f603acf9f9911bfa043649e
Joey Zhou <jfzhou.nc@gmail.com>
2019-07-22 22:33:46 -0400

    expandable card

--- components/Project.js
+++ components/Project.js
@@ -1,17 +1,80 @@
 import React from 'react';
 import styled from 'styled-components';
+import {
+  Animated,
+  TouchableWithoutFeedback,
+  Dimensions,
+  StatusBar,
+  TouchableOpacity,
+} from 'react-native';
+import * as Icon from '@expo/vector-icons';
+
+const screenWidth = Dimensions.get('window').width;
+const screenHeight = Dimensions.get('window').height;
+const tabBarHeight = 75;
 
 class Project extends React.Component {
+  state = {
+    cardWidth: new Animated.Value(315),
+    cardHeight: new Animated.Value(460),
+    titleTop: new Animated.Value(20),
+    opacity: new Animated.Value(0)
+  };
+
+  openCard = () => {
+    Animated.spring(this.state.cardWidth, { toValue: screenWidth }).start();
+    Animated.spring(this.state.cardHeight, { toValue: screenHeight - tabBarHeight }).start();
+
+    Animated.spring(this.state.titleTop, { toValue: 40 }).start();
+    Animated.spring(this.state.opacity, { toValue: 100 }).start();
+
+    StatusBar.setHidden(true);
+  };
+
+  closeCard = () => {
+    Animated.spring(this.state.cardWidth, { toValue: 315 }).start();
+    Animated.spring(this.state.cardHeight, { toValue: 460 }).start();
+
+    Animated.spring(this.state.titleTop, { toValue: 20 }).start();
+    Animated.spring(this.state.opacity, { toValue: 0 }).start();
+
+    StatusBar.setHidden(false);
+  };
+
   render() {
     return (
-      <Container>
-        <Cover>
-          <Image source={this.props.image} />
-          <Title>{this.props.title}</Title>
-          <Author>by {this.props.author}</Author>
-        </Cover>
-        <Text>{this.props.text}</Text>
-      </Container>
+      <TouchableWithoutFeedback onPress={this.openCard}>
+        <AnimatedContainer style={{ width: this.state.cardWidth, height: this.state.cardHeight }}>
+          <Cover>
+            <Image source={this.props.image} />
+            <AnimatedTitle
+              style={{
+                top: this.state.titleTop,
+              }}
+            >
+              {this.props.title}
+            </AnimatedTitle>
+            <Author>by {this.props.author}</Author>
+          </Cover>
+          <Text>{this.props.text}</Text>
+          <TouchableOpacity
+            style={{
+              position: "absolute",
+              top: 20,
+              right: 20
+            }}
+            onPress={this.closeCard}
+          >
+            <AnimatedCloseView
+              style={{
+                opacity: this.state.opacity
+              }}
+            >
+              <Icon.Ionicons name="ios-close" size={32} color="#546bfb" />
+            </AnimatedCloseView>
+          </TouchableOpacity>
+        </AnimatedContainer>
+      </TouchableWithoutFeedback>
     );
   }
 }
@@ -25,6 +88,19 @@ const Container = styled.View`
   background-color: white;
   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.15);
 `;
+
+const AnimatedContainer = Animated.createAnimatedComponent(Container);
+
+const CloseView = styled.View`
+  width: 32px;
+  height: 32px;
+  background: white;
+  border-radius: 16px;
+  justify-content: center;
+  align-items: center;
+`;
+
+const AnimatedCloseView = Animated.createAnimatedComponent(CloseView);
 
 const Cover = styled.View`
   height: 290px;
@@ -47,6 +123,8 @@ const Title = styled.Text`
   color: white;
   width: 300px;
 `;
+
+const AnimatedTitle = Animated.createAnimatedComponent(Title);
 
 const Author = styled.Text`
   position: absolute;
